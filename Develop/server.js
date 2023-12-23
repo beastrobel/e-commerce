4e95b5fce2bf833213da575294c6197c2c5e1f90
beastrobel <beatrizstrobel@proton.me>
2023-12-22 19:34:42 -0500

    Added catch-all route to server.js, deleted route from index.js

--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+//Catch-all route
+app.get('/', (req, res) => {
+  res.send('<h1>Homepage</h1>')
+});
+
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
