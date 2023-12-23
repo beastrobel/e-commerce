25bbcffb28ad1dde8b7cd7b5509cdd294cf37cdc
beastrobel <beatrizstrobel@proton.me>
2023-12-23 00:02:57 -0500

    Delete routes complete

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -5,7 +5,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   Tag.findAll({
-    include: [{model: Product}, {model: ProductTag}],
+    include: [{model: Product}],
   })
   .then((tags)=>{
     res.send(tags)
@@ -17,8 +17,19 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{model: Product}],
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tags)=>{
+    res.send(tags)
+  }).catch(err => {
+    if (err) {
+      console.log(err);
+    };
+  });
 });
 
 router.post('/', (req, res) => {
@@ -30,7 +41,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
