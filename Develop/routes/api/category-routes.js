385ed71120dcc861f8bf6a6e6015671443579acf
beastrobel <beatrizstrobel@proton.me>
2023-12-22 23:37:05 -0500

    Category GET routes are working properly

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,7 +4,10 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  Category.findAll().then((categories)=>{
+  Category.findAll({
+    include: [{model: Product}],
+  })
+  .then((categories)=>{
     res.send(categories)
   }).catch(err => {
     if (err) {
@@ -14,13 +17,15 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  Category.findAll({ where: id === parseInt(req.params.id)}).then((categories)=>{
-    res.send(categories)
-  }).catch(err => {
-    if (err) {
-      console.log(err);
-    };
-  });
+  Category.findAll({
+  include: [{model: Product}],
+  where: {
+    id: req.params.id
+  }
+})
+.then((category) => {
+  res.json(category);
+});
   // find one category by its `id` value
   // be sure to include its associated Products
 });
@@ -28,21 +33,43 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new category
   Category.create({
-    category_name: 'pants'
-  }).catch(err => {
-    if (err) {
-      console.log(err);
-    };
-  });
-  res.send('New category created');
+    category_name: req.body.category_name,
+  })
+  .then((newCategory) => {
+    res.json(newCategory);
+  })
+  .catch((err) => {
+    res.json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((updatedCategory) => {
+    res.json(updatedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
